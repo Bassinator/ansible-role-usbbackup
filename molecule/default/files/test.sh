3e749cde63b907a2fc36ef3017d66a52fdef16a5
Bastian Bukatz <bastian.greuel@gmail.com>
2019-02-27 23:35:32 +0100

    added installtion of vfat for debian and redhat

--- molecule/default/files/test.sh
+++ molecule/default/files/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 dd if=/dev/zero of=image bs=1M count=1024
-mkfs.ext4 image
+mkfs.vfat image
 mkdir /mnt/usbbackup
 mount -o loop image /mnt/usbbackup
 touch /mnt/usbbackup/ttt
